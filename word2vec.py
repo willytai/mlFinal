1adc14895a473879f2f4a92cad571c9ec380bc22
Willy Tai <b04502139@ntu.edu.tw>
2018-06-09 18:00:01 +0800

    updated saving method for word2vec

--- word2vec.py
+++ word2vec.py
@@ -1,33 +1,43 @@
 from gensim.models import word2vec
+from gensim.models.keyedvectors import KeyedVectors
 
 
 def main():
-	sentences = list(word2vec.LineSentence('dataset/segmentated/1_train_seg.txt'))
+	sentences  = list(word2vec.LineSentence('dataset/segmentated/1_train_seg.txt'))
 	sentences += list(word2vec.LineSentence('dataset/segmentated/2_train_seg.txt'))
 	sentences += list(word2vec.LineSentence('dataset/segmentated/3_train_seg.txt'))
 	sentences += list(word2vec.LineSentence('dataset/segmentated/4_train_seg.txt'))
 	sentences += list(word2vec.LineSentence('dataset/segmentated/5_train_seg.txt'))
+	sentences += list(word2vec.LineSentence('dataset/segmentated/test_seg.txt'))
 
-	model = word2vec.Word2Vec(sentences, size=300)
+	model = word2vec.Word2Vec(sentences, size=100, min_count=3, iter=20, workers=-1)
 
-	model.save('word2vec.model')
+	print ('vocab_size:', model.wv.syn0.shape[0])
+
+	word_vec = model.wv
+
+	word_vec.save_word2vec_format('word2vec.bin', binary=True)
 
 	####################
 	## test similarity
 	####################
 
-	model = word2vec.Word2Vec.load('word2vec.model')
+	word_vectors = KeyedVectors.load_word2vec_format('word2vec.bin', binary=True)
+
+	print ('successfully loaded')
+
+	# model = word2vec.Word2Vec.load('word2vec.model')
 	
-	test = "風火輪"
+	# test = "風火輪"
 
-	buf = model.most_similar(test, topn=10)
+	# buf = model.most_similar(test, topn=10)
 
-	print (test)
+	# print (test)
 
-	print ("相似詞前 10 排序")
+	# print ("相似詞前 10 排序")
 
-	for item in buf:
-		print ('{}, {}'.format(item[0], item[1]))
+	# for item in buf:
+	# 	print ('{}, {}'.format(item[0], item[1]))
 
 
 if __name__ == '__main__':
